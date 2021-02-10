96b7f0e82a6b2a30e617125fe83ba4ecd6090ab3
mpathakamuri11 <78628672+mpathakamuri11@users.noreply.github.com>
2021-02-09 21:13:17 -0500

    commit all

---
+++ src/Router/About.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            
+            <h1>developed by Madhuri</h1>
+         );
+    }
+}
+ 
+export default About;
